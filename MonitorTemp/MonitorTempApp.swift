e5fb885f4b733e77fb0ff4a02f2f3a752bca0b8e
Raymundo Perez <perez_prz_ray@hotmail.com>
2026-04-05 22:22:38 -0600

    Initial Commit

---
+++ MonitorTemp/MonitorTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitorTempApp.swift
+//  MonitorTemp
+//
+//  Created by Raymundo Perez Perez on 05/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MonitorTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
